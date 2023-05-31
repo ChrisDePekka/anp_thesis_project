aba99b4ff8df93c548ae53faf2fdf2838649278f
ChrisDePekka <Christian15800@gmail.com>
2023-05-31 12:10:30 +0200

    update2

--- model.py
+++ model.py
@@ -13,51 +13,51 @@ from constants import n_s, n_g_r, lm_model, ls_eval_aspect, path, path2
 
 if __name__ == "__main__":
 
-    # a = 1
-    # if a == 1:
+    a = 1
+    if a == 1:
 
 
-        #Import small dataset to test the prompts on
-    dataset_to_generate, df_0 = get_data()
-    df_0.to_csv('df_0.csv', index=False)
-    #print_full(dataset_to_generate)
+            #Import small dataset to test the prompts on
+        dataset_to_generate, df_0 = get_data()
+        df_0.to_csv('df_0.csv', index=False)
+        #print_full(dataset_to_generate)
+        
+        # # Use only the first row to show that it works
+        sample_row_df = dataset_to_generate.iloc[2:4]
+        print(sample_row_df)
+        sample_row_df = sample_row_df.reset_index(drop=True)
+        
+        # # dataframe including the prompt_newsarticle combination
+        df_with_prompts_incl_prompt_news = create_prompt_newsarticle(sample_row_df)
+
+        df_with_generated_radio = generate_radio_messages(df_with_prompts_incl_prompt_news)
+
+        df_1 = df_with_generated_radio.copy()
+        df_1.to_csv('df_1.csv', index=False)
+
+    else:
     
-    # # Use only the first row to show that it works
-    sample_row_df = dataset_to_generate.iloc[2:4]
-    print(sample_row_df)
-    sample_row_df = sample_row_df.reset_index(drop=True)
-    
-    # # dataframe including the prompt_newsarticle combination
-    df_with_prompts_incl_prompt_news = create_prompt_newsarticle(sample_row_df)
+        df_1_cont = pd.read_csv(f'{path2}/anp_thesis_project/df_1.csv', delimiter=',')
 
-    df_with_generated_radio = generate_radio_messages(df_with_prompts_incl_prompt_news)
+        
+        df_2_int = create_df(df_1_cont)
 
-    df_1 = df_with_generated_radio.copy()
-    df_1.to_csv('df_1.csv', index=False)
+        df_2 = create_eval_prompts(df_1_cont)
+        df_2.to_csv('df_2.csv', index=False)
+        df_2_cont = pd.read_csv(f'{path2}/anp_thesis_project/df_2.csv', delimiter=',')
 
-    #else:
-    
-    df_1_cont = pd.read_csv(f'{path2}/anp_thesis_project/df_1.csv', delimiter=',')
+            #if a == 3:
+        df_3 = generate_radio_scores(df_2_cont, df_2_int)
+        df_3.to_csv('df_3.csv', index=False)
+        
 
-    
-    df_2_int = create_df(df_1_cont)
+        df_3_cont = pd.read_csv(f'{path2}/anp_thesis_project/df_3.csv', delimiter=',')
 
-    df_2 = create_eval_prompts(df_1_cont)
-    df_2.to_csv('df_2.csv', index=False)
-    df_2_cont = pd.read_csv(f'{path2}/anp_thesis_project/df_2.csv', delimiter=',')
-
-        #if a == 3:
-    df_3 = generate_radio_scores(df_2_cont, df_2_int)
-    df_3.to_csv('df_3.csv', index=False)
-    
-
-    df_3_cont = pd.read_csv(f'{path2}/anp_thesis_project/df_3.csv', delimiter=',')
-
-    df_4 = post_processing_1(df_3_cont)
+        df_4 = post_processing_1(df_3_cont)
 
 
-    df_4_c = df_4.copy()
-    df_4_c.to_csv('df_4.csv', index=False)
+        df_4_c = df_4.copy()
+        df_4_c.to_csv('df_4.csv', index=False)
 
 
 
